9e31fa28e6213a41df8263da781701a89adc0833
koojaa <93jwkoo@gmail.com>
2020-02-28 23:17:37 +0900

    match template && component structurized

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import MatchTemplate from './MatchTemplate/MatchTemplate';
 
 class App extends Component {
 	render() {
 		return (
 			<div>
-				<h1>hello world!</h1>
+				<MatchTemplate />
 			</div>
 		);
 	}
