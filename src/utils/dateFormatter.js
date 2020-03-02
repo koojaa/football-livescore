00a6300aa2429e2d596f74114d5185ef9079dc17
kooja <93jwkoo@gmail.com>
2020-03-02 15:35:15 +0900

    config file added

--- src/utils/dateFormatter.js
+++ src/utils/dateFormatter.js
@@ -1,5 +1,5 @@
 export default function dateFormatter(date) {
-	var d = new Date(date),
+	let d = new Date(date),
 		month = '' + (d.getMonth() + 1),
 		day = '' + d.getDate(),
 		year = d.getFullYear();
